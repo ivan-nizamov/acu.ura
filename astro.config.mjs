28d8300fc3e76941b08165467da820c37f21ea13
navi <ivan.nizamov@proton.com>
2025-11-01 12:46:00 +0200

    Add dropdown navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import tailwindcss from "@tailwindcss/vite";
 // tailwind.config.mjs
 import { defineConfig } from 'astro/config';
 
+import icon from "astro-icon";
+
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [icon()],
 
   // toggled via <html class="dark">
   darkMode: ['class'],
